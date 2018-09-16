aa662131ef48b75abe4f21e950061c1a12772bb0
Administrator <mickeysaint@163.com>
2018-09-16 14:05:53 +0800

    bb

--- src/anew.java
+++ src/anew.java
@@ -2,6 +2,12 @@ public class anew {
 
     public static void main(String[] args) {
         System.out.println("hello1");
+
+        int i;
+        i=0;
+        i++;
+        System.out.println(i);
+
     }
 
 }
